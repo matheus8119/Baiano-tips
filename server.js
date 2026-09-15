594bd61cf6dc4c0163ff675f3c87402b03a13659
matheus8119 <goncalvesmathheus@gmail.com>
2026-09-15 16:15:55 -0300

    Serve index.html from public directory

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("BAIANO TIPS ONLINE");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
